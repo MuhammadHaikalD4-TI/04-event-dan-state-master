0df41eb0c8d0d6613d3f0ab50d53fb4f33c8433d
Muhammad Haikal Buldan <h.buldan246@gmail.com>
2024-04-30 07:10:17 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### Penjelasan Praktikum 2🎅
+
+Fungsi Tombol_2 memiliki 2 parameter yaitu isiPesan dan namaTombol, pada <b>page.tsx </b> pemanggilan <i>Tombol_2</i> diikuti dengan isi untuk kedua parameter, yaitu "Ini Pesanku" dan nama tombol "Pesan"
+
+### Penjelasan Praktikum 4
+
+1. Akan terjadi error ketika menekan artikel selanjutnya setelah artikel ke 5.
+2. Hasil sesudah di fix :
+![alt text](image.png)
+
+### Penjelasan Praktikum 5
+
+1. Tidak ada perbedaan
+2. Untuk menghindari redundan dan bug pada aplikasi react
+
+### Penjelasan Praktikum 6
---
+++ src/components/button.tsx
@@ -0,0 +1,42 @@
+export default function Tombol_1() {
+    
+    function handleClick() {
+        alert("Tombol di tekan!!!!");
+    }
+
+    function handleMouseOver() {
+        alert("Hayolo, mau pencet tombol ya?");
+    }
+    
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        >
+            Ini tombol mas
+        </button>
+    )
+}
+
+export function Tombol_2({isiPesan, namaTombol}) {
+    return (
+        <button
+            className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+            onClick={() => alert(isiPesan)}>
+            {namaTombol}
+        </button>
+    )
+}
+
+export function Tombol_3({isiPesan, namaTombol}) {
+    return (
+        <button
+            className="bg-green-400 hover:bg-green-700 text-white p-2 rounded m-2"
+            onClick={(e) => {
+                e.stopPropagation()
+                alert(isiPesan)
+            }}>
+            {namaTombol}
+        </button>
+    )
+}
